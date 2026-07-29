c8866a7cef18b6024cd512115c24f8facbc698b0
blacksheep12780 <amal628277@gmail.com>
2026-07-29 20:08:34 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Blood leaves a trace. So does revenge", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        type: 3,               // Custom
       },
     ],
   },
